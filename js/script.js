bdc871d7df71a396bc7bf9a00d4937a7a9555933
felon91 <felon_91@mail.ru>
2019-12-05 22:44:26 +0300

    - added script top open and close menu - added notification messages

--- js/script.js
+++ js/script.js
@@ -1,15 +1,81 @@
-const $authBlocks = document.querySelectorAll('.reg-auth__block');
-document.querySelector('[data-type="reg"]').addEventListener('animationend', addActiveClass);
-document.querySelector('.reg-auth').addEventListener('click', {handleEvent: addInActiveClass, 'authBlocks': $authBlocks}, addInActiveClass);
+document.querySelector('#registration').addEventListener('submit', registration);
+document.querySelector('#authorization').addEventListener('submit', authorization);
+document.querySelector('#reset').addEventListener('submit', resetPassword);
 
-function addActiveClass(evt) {
-  console.log(evt);
+Array.from(document.querySelectorAll('.switchForm')).forEach(function(item) {
+  item.addEventListener('click', slideBlock);
+});
+
+const labels = {
+  'errorEmail': 'Учетной записи с таким email не существует',
+  'successReset': 'Мы отправили письмо на ivanov@gmail.com. Перейдите по ссылке в письме, чтобы сменить пароль.',
+};
+
+function toggleClass(firstBlock, secondBlock) {
+  document.querySelector(`[data-type="${firstBlock}"]`).classList.remove('active');
+  document.querySelector(`[data-type="${firstBlock}"]`).classList.add('inactive');
+  setTimeout(function() {
+    document.querySelector(`[data-type="${secondBlock}"]`).classList.remove('inactive');
+    document.querySelector(`[data-type="${secondBlock}"]`).classList.add('active');
+  }, 500);
 }
 
-function addInActiveClass(evt, elements) {
+function slideBlock(evt) {
   evt.preventDefault();
-  console.log(evt, this);
-  elements.forEach(function(item) {
-    item.classList.add('inactive');
-  });
+  const blockToBeOpened = evt.target.getAttribute('href').slice(1);
+  const typeCurrentBlock = evt.target.dataset.block;
+
+  switch (blockToBeOpened) {
+    case 'auth': {
+      toggleClass(typeCurrentBlock, blockToBeOpened);
+      break;
+    }
+    case 'reg': {
+      toggleClass(typeCurrentBlock, blockToBeOpened);
+      break;
+    }
+    case 'reset': {
+      toggleClass(typeCurrentBlock, blockToBeOpened);
+      break;
+    }
+  }
 }
+
+function registration(evt) {
+  evt.preventDefault();
+  const email = this.querySelector('.email').value;
+  const password = this.querySelector('.pass').value;
+  alert(`Регистрация - Email: ${email}, Пароль: ${password}`);
+}
+
+function authorization(evt) {
+  evt.preventDefault();
+  const email = this.querySelector('.email').value;
+  const password = this.querySelector('.pass').value;
+  alert(`Авторизация - Email: ${email}, Пароль: ${password}`);
+}
+
+function resetPassword(evt) {
+  evt.preventDefault();
+  const email = this.querySelector('.email').value;
+  alert(`Восстановление: Email: ${email}`);
+}
+
+function showNotification(options) {
+  const $notificationElement = options.typeBlock.querySelector('.reg-auth__notification');
+  $notificationElement.classList.add(options.class);
+  $notificationElement.innerHTML = `<p class="${options.class}__text">${options.message}</p>`;
+  setTimeout(function () {
+    $notificationElement.classList.add('hide');
+    setTimeout(function () {
+      $notificationElement.classList.remove('hide');
+      $notificationElement.innerHTML = '';
+    }, 500);
+  }, 5000);
+}
+
+showNotification({
+  'typeBlock': document.querySelector('[data-type="reg"]'),
+  'class': 'error',
+  'message': labels.errorEmail
+});
